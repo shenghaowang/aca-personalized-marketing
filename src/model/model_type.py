a7054ec65f9a8a03883a4ac2f34b2d5b2f8883bf
Shenghao Wang <wangshenghao1993@gmail.com>
2025-10-15 23:42:53 -0400

    Add Uplift Random Forest model

--- src/model/model_type.py
+++ src/model/model_type.py
@@ -1,26 +1,45 @@
 from enum import Enum
 from typing import Union
 
+from causalml.inference.tree import UpliftRandomForestClassifier
 from lightgbm import LGBMClassifier
 
 from model.neuralnet import NeuralNetClassifier
 
 
 class ModelType(str, Enum):
-    LGBM = "lgbm"
-    MLP = "mlp"
+    ClassTransformLGBM = "lgbm"
+    ClassTransformMLP = "mlp"
+    UpliftRF = "uplift_rf"
+
+
+def get_model_kwargs(model_type: str, feature_cols: list) -> dict:
+    """Get the required kwargs for initializing a model based on its type."""
+    match model_type:
+        case ModelType.ClassTransformLGBM.value | ModelType.ClassTransformMLP.value:
+            return {"input_dim": len(feature_cols)}
+        case ModelType.UpliftRF.value:
+            return {"control_name": "No"}
+        case _:
+            return {}
 
 
 def init_model(
-    model_name: str, input_dim: int = None
-) -> Union[LGBMClassifier, NeuralNetClassifier]:
+    model_name: str, **kwargs
+) -> Union[LGBMClassifier, NeuralNetClassifier, UpliftRandomForestClassifier]:
     match model_name:
-        case ModelType.LGBM.value:
+        case ModelType.ClassTransformLGBM.value:
             model = LGBMClassifier(
                 random_state=42, scale_pos_weight=5, learning_rate=0.01, n_estimators=50
             )
-        case ModelType.MLP.value:
-            model = NeuralNetClassifier(input_dim=input_dim)
+        case ModelType.ClassTransformMLP.value:
+            model = NeuralNetClassifier(input_dim=kwargs.get("input_dim"))
+        case ModelType.UpliftRF.value:
+            model = UpliftRandomForestClassifier(
+                n_estimators=10,
+                control_name=kwargs.get("control_name"),
+                random_state=42,
+            )
         case _:
             raise ValueError(f"Unsupported model type: {model_name}")
 
