e4392b402f79eeb7b78bda880f2f8a7139e32d03
Shenghao Wang <wangshenghao1993@gmail.com>
2025-10-15 23:44:19 -0400

    Keep the original treatment column

--- src/data/starbucks.py
+++ src/data/starbucks.py
@@ -7,14 +7,20 @@ def load_data(train_path: Path, test_path: Path) -> tuple[pd.DataFrame, pd.DataF
     train_df = pd.read_csv(train_path)
     test_df = pd.read_csv(test_path)
 
-    train_df["Promotion"] = train_df["Promotion"].map({"Yes": 1, "No": 0})
-    test_df["Promotion"] = test_df["Promotion"].map({"Yes": 1, "No": 0})
+    # Create binary treatment variable
+    train_df["treatment"] = train_df["Promotion"].apply(
+        lambda x: 1 if x == "Yes" else 0
+    )
+    test_df["treatment"] = test_df["Promotion"].apply(lambda x: 1 if x == "Yes" else 0)
 
-    train_df["label"] = train_df.apply(
-        lambda row: 1 if row.Promotion == row.purchase else 0, axis=1
-    )
-    test_df["label"] = test_df.apply(
-        lambda row: 1 if row.Promotion == row.purchase else 0, axis=1
-    )
+    train_df["label"] = (
+        ((train_df["treatment"] == 1) & (train_df["purchase"] == 1))
+        | ((train_df["treatment"] == 0) & (train_df["purchase"] == 0))
+    ).astype(int)
+
+    test_df["label"] = (
+        ((test_df["treatment"] == 1) & (test_df["purchase"] == 1))
+        | ((test_df["treatment"] == 0) & (test_df["purchase"] == 0))
+    ).astype(int)
 
     return train_df, test_df
