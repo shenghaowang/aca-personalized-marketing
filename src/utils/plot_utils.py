f3884b2867d3a8079299958a90d0a2942b3323da
Shenghao Wang <wangshenghao1993@gmail.com>
2025-11-16 11:26:12 -0500

    Add helper method for plotting percentage decrease of metrics

--- src/utils/plot_utils.py
+++ src/utils/plot_utils.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import List
 
 import matplotlib.pyplot as plt
@@ -8,11 +9,12 @@ colors = {"lgbm": "red", "mlp": "blue", "uplift_rf": "orange"}
 
 
 def plot_pct_decrease(
-    avg: List[np.ndarray],
-    ci_low: List[np.ndarray],
-    ci_high: List[np.ndarray],
     model_names: List[str],
     title: str,
+    output_path: Path,
+    avg: List[np.ndarray],
+    ci_low: List[np.ndarray] = None,
+    ci_high: List[np.ndarray] = None,
 ):
     plt.figure(figsize=(10, 7))
     frac = np.array(range(11)) / 100
@@ -20,13 +22,19 @@ def plot_pct_decrease(
     # Plot Qini coefficients
     for i, model_name in enumerate(model_names):
         plt.plot(frac, avg[i], label=model_labels[model_name], color=colors[model_name])
-        plt.fill_between(
-            frac, ci_low[i], ci_high[i], color=colors[model_name], alpha=0.2
-        )
+
+        if ci_low is not None and ci_high is not None:
+            plt.fill_between(
+                frac, ci_low[i], ci_high[i], color=colors[model_name], alpha=0.2
+            )
 
     plt.xlabel("Fraction of participants")
     plt.ylabel("Percentage decrease (%)")
     plt.title(title)
     plt.grid(True)
     plt.legend()
-    plt.show()
+
+    # Save figure
+    plt.tight_layout()
+    plt.savefig(output_path)
+    plt.close()
